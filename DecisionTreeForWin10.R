8883c67e32efde2ef1c026200b65bb071ab78032
CT <uhjio1233@gmail.com>
2020-05-08 13:03:41 +0800

    old data

--- DecisionTreeForWin10.R
+++ DecisionTreeForWin10.R
@@ -4,9 +4,9 @@ library("rpart.plot")
 library("rattle")
 
 #載入資料(設定工作目錄,資料及存放地)
-getwd()
+
 setwd("D:/r place")
-getwd()
+
 #測試模型,可隨機產生(訓練資料,測試資料)
 traindata=read.csv("Parkinsons_Train.csv")
 testdata=read.csv("Parkinsons_Test.csv")
@@ -39,34 +39,35 @@ mycolName[1]
 #(6A)正確率
 #計算正確率(precision)
 precision <- (cm[[1]]/sum(cm[,1]))
-paste("預測",mycolName[1],"的正確率,precision=",precision)
-
+st<-paste("預測",mycolName[1],"的正確率,precision=",precision)
+print(st)
 #計算正確率(TPR)
 TPR <- (cm[[1]]/sum(cm[1,]))
-paste("預測",mycolName[1],"的正確率,TPR=",TPR)
-
+st<-paste("預測",mycolName[1],"的正確率,TPR=",TPR)
+print(st)
 #計算正確率(TNR)
-TNR<-(cm[[4]]/sum(cm[2,]))
-paste("預測",mycolName[2],"的正確率,TNR=",TNR)
-
+TNR<-(cm[[1]]/sum(cm[1,]))
+st<-paste("預測",mycolName[1],"的正確率,TNR=",TNR)
+print(st)
 #(6B)正確率
 #計算正確率(precision)
-precision <- (cm[4]/sum(cm[,2]))
-paste("預測",mycolName[1],"的正確率,=precision",precision)
-
+precision <- (cm[[4]]/sum(cm[,2]))
+st<-paste("預測",mycolName[2],"的正確率,=precision",precision)
+print(st)
 #計算正確率(TPR)
 TPR <- (cm[[4]]/sum(cm[2,]))
-paste("預測",mycolName[2],"的正確率,TPR=",TPR)
-
+st<-paste("預測",mycolName[2],"的正確率,TPR=",TPR)
+print(st)
 #計算正確率(TNR)
 TNR <- (cm[[4]]/sum(cm[2,]))
-paste("預測",mycolName[2],"的正確率,TNR=",TNR)
-
+st<-paste("預測",mycolName[2],"的正確率,TNR=",TNR)
+print(st)
 
 #整體準確率(取出對角/總數)
 accuracy <- sum(diag(cm))/sum(cm)
-paste("整體準確率=",accuracy)
-paste("整體準確率=",round(accuracy,2))
-
+st<-paste("整體準確率=",accuracy)
+print(st)
+st<-paste("整體準確率=",round(accuracy,2))
+print(st)
 
 
